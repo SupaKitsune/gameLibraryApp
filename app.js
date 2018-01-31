cdfc6869c38838c771d5a9d16f1b8399515f9fc9
Fox <jcfox@labs.neit.edu>
2018-01-31 12:57:59 -0500

    database now stays after shutdown

--- app.js
+++ app.js
@@ -3,22 +3,34 @@ var path = require('path')
 var express = require('express')
 var logger = require('morgan')
 var bodyParser = require('body-parser')
+var MongoClient = require('mongodb').MongoClient
+var url = "mongodb://localhost:27017/"
 
 var app = express()
 
 app.set('views', path.resolve(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-var entries = []
+//var entries = []
 
-app.locals.entries = entries;
+//app.locals.entries = entries;
 
 app.use(logger('dev'))
 
 app.use(bodyParser.urlencoded({extended:false}))
 
 app.get("/", function(request,response){
-	response.render("index")
+	MongoClient.connect(url, function(error, db){
+		if(error)throw error;
+		var dbObj = db.db("games");
+		
+		dbObj.collection("games").find().toArray(function(error, result){
+			if(error)throw error;
+			console.log("Site served")
+			db.close()
+			response.render("index", {games:result})
+		})
+	})
 })
 
 app.get("/new-entry", function(request,response){
@@ -27,15 +39,30 @@ app.get("/new-entry", function(request,response){
 
 app.post("/new-entry", function(request, response){
 	if(!request.body.title || !request.body.body){
-		response.status(400).send("EFF/n(empty entry forbidden)")
+		response.status(400).send("EEF/n(empty entry forbidden)")
 		return;
 	}
+	
+	MongoClient.connect(url, function(error, db){
+		if(error) throw error;
+		
+		var dbObj = db.db("games");
+		
+		dbObj.collection("games").save(request.body, function(error, result){
+			console.log("Database " + dbObj.name + " save made.")
+			db.close()
+			response.redirect("/")
+		})
+	})
+	
+	/*
 	entries.push({
 		title:request.body.title,
 		body:request.body.body,
 		published:new Date()
 	})
-	response.redirect("/")
+	*/
+	//response.redirect("/")
 })
 
 app.use(function(request, response){
